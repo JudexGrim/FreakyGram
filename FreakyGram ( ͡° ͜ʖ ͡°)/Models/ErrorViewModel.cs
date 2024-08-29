7a7c783308c9a98aeb3406cb719ef29bad655f33
hamod <hamod@DESKTOP-V1P9SNP>
2024-08-29 21:32:56 +0300

    Add project files.

---
+++ FreakyGram ( ͡° ͜ʖ ͡°)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreakyGram___͡__͜ʖ_͡__.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
